e56c11ccc78580e843f24c99218ec2c3034ab0aa
kennzuki <kingsyken@gmail.com>
2025-11-19 03:54:31 +0300

    constants root layout and variables

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Kenki Store"
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Kenki Store" 
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "https://localhost:3000"
